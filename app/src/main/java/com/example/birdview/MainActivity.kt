bacc7922bd762e4c2f3a06ee9bb14e021a3cc90d
ST10091894 <ST10091894@vcconnect.edu.za>
2023-09-14 17:09:20 +0200

    Changed activities to fragments Created Navigation bar in Main Activity

--- app/src/main/java/com/example/birdview/MainActivity.kt
+++ app/src/main/java/com/example/birdview/MainActivity.kt
@@ -1,26 +1,46 @@
 package com.example.birdview
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.birdview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val btn_sign_in : Button = findViewById(R.id.btn_sign_in)
-        val btn_sign_up : Button = findViewById(R.id.btn_sign_up)
 
-        btn_sign_in.setOnClickListener(){
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+/*
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
 
-        btn_sign_up.setOnClickListener(){
-            val intent = Intent(this, ObservationsActivity::class.java)
-            startActivity(intent)
-        }
+         */
     }
+
+    /*
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+     */
 }
---
+++ app/src/main/java/com/example/birdview/SplashPageActivity.kt
@@ -0,0 +1,26 @@
+package com.example.birdview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SplashPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_page)
+
+        val btn_sign_in : Button = findViewById(R.id.btn_sign_in)
+        val btn_sign_up : Button = findViewById(R.id.btn_sign_up)
+
+        btn_sign_in.setOnClickListener(){
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_sign_up.setOnClickListener(){
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
