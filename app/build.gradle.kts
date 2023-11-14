e9844d02585a52f7621459f97b6ea0395a25033c
st10092299 <st10092299@vcconnect.edu.za>
2023-11-14 06:34:50 +0200

    Recording functionality for Manual Entry and Unidentified Species.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
 
     // Glide for image loading
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
 
     testImplementation("junit:junit:4.13.2")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
--- app/src/main/java/com/example/birdview/UnidentifiedDialogFragment.kt
+++ app/src/main/java/com/example/birdview/UnidentifiedDialogFragment.kt
@@ -8,24 +8,33 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.Button
+import android.widget.Chronometer
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.storage
+import java.io.File
 import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -37,8 +46,28 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
         const val REQUEST_OPEN_IMAGE = 102
     }
 
-    private lateinit var image_bird : ImageView
-    private var encodedBitmap : String? = null
+    private var mediaRecorder: MediaRecorder? = null
+    private lateinit var recordingChronometer: Chronometer
+    private lateinit var playbackChronometer: Chronometer
+    private var mediaPlayer: MediaPlayer? = null
+    private var isRecording = false
+    private var isPlaying = false
+    private var tempFile: File? = null
+    private val RECORD_AUDIO_PERMISSION = android.Manifest.permission.RECORD_AUDIO
+    private val WRITE_EXTERNAL_STORAGE_PERMISSION =
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    private val PERMISSION_REQUEST_CODE = 123
+    private val storage = Firebase.storage
+    private lateinit var image_bird: ImageView
+    private lateinit var tv_record_sound: TextView
+    private lateinit var tv_play_sound: TextView
+    private lateinit var btnRecord: LinearLayout
+    private lateinit var btnPlay: LinearLayout
+    private var encodedBitmap: String? = null
+    private lateinit var imgTakePicture : LinearLayout
+    private lateinit var imgUploadImage : LinearLayout
+    private lateinit var btnOK : Button
+    private lateinit var btnCancel : Button
 
     override fun getTheme(): Int {
         return R.style.AppBottomSheetDialogTheme
@@ -54,13 +83,32 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
             getDialog()?.getWindow()?.requestFeature(Window.FEATURE_NO_TITLE)
         }
 
-        val imgTakePicture = view.findViewById<LinearLayout>(R.id.lyt_take_picture)
-        val imgUploadImage = view.findViewById<LinearLayout>(R.id.lyt_upload_image)
-        val btnOK = view.findViewById<Button>(R.id.btnOk)
-        val btnCancel = view.findViewById<Button>(R.id.btnCancel)
+        imgTakePicture = view.findViewById<LinearLayout>(R.id.lyt_take_picture)
+        imgUploadImage = view.findViewById<LinearLayout>(R.id.lyt_upload_image)
+        btnOK = view.findViewById<Button>(R.id.btnOk)
+        btnCancel = view.findViewById<Button>(R.id.btnCancel)
         image_bird = view.findViewById<ImageView>(R.id.img_bird)
 
-        btnCancel.setOnClickListener{
+        tv_record_sound = view.findViewById(R.id.tv_record_sound)
+        tv_play_sound = view.findViewById(R.id.tv_play_sound)
+        mediaPlayer = MediaPlayer()
+        mediaRecorder = MediaRecorder()
+        btnRecord = view.findViewById(R.id.lyt_record_sound)
+        btnPlay = view.findViewById(R.id.lyt_play_sound)
+        recordingChronometer = view.findViewById(R.id.recording_chronometer)
+        recordingChronometer.base = SystemClock.elapsedRealtime()
+        playbackChronometer = view.findViewById(R.id.playback_chronometer)
+        playbackChronometer.base = SystemClock.elapsedRealtime()
+        btnRecord.setOnClickListener {
+            //Toast.makeText(context, "Hello", Toast.LENGTH_SHORT).show()
+            toggleRecording()
+        }
+//
+        btnPlay.setOnClickListener {
+            togglePlaySound()
+        }
+
+        btnCancel.setOnClickListener {
             dismiss()
         }
 
@@ -73,6 +121,8 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
                 btnCancel.isEnabled = false
                 imgTakePicture.isEnabled = false
                 imgUploadImage.isEnabled = false
+                btnRecord.isEnabled = false
+                btnPlay.isEnabled = false
                 val user = FirebaseAuth.getInstance().currentUser
                 try {
                     //code attribution
@@ -83,28 +133,50 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
                     val localDateTime = LocalDateTime.now()
                     val formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy HH:mm")
                     val output = localDateTime.format(formatter)
-                    val obs : Observation = Observation(null, encodedBitmap, "Unspecified Species", "I don't know", null, latitude, longitude, output)
-                    //GlobalVariablesMethods.user.categories?.add(cat)
-                    val database = FirebaseDatabase.getInstance()
-                    val databaseReference = database.getReference("Users")
-                    //code attribution
-                    //the following code was taken from Stack Overflow and adapted
-                    //https://stackoverflow.com/questions/60432256/on-insert-data-in-firebase-realtime-database-it-deletes-previous-data
-                    //ashok
-                    //https://stackoverflow.com/users/12746098/ashok
-                    databaseReference.child(user?.uid.toString()).child("observations").push().setValue(obs).addOnCompleteListener() {
-                        if (it.isComplete){
-                            Toast.makeText(context, "Observation added successfully.", Toast.LENGTH_SHORT).show()
-                        }
-                        else{
-                            Toast.makeText(context, "Observation could not be added.", Toast.LENGTH_SHORT).show()
-                        }
-                    }.addOnCompleteListener(){
-                        dismiss()
-                    }.addOnFailureListener(){
-                        Toast.makeText(context, "Failure", Toast.LENGTH_SHORT).show()
-                    }
+                    tempFile?.let { file ->
+                        // Upload the file to Firebase Storage
+                        val storageRef = storage.reference.child("${user?.uid.toString()}/audio/${file.name}")
+                        val uploadTask = storageRef.putFile(Uri.fromFile(file))
 
+                        uploadTask.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                // File upload successful
+                                // You can get the download URL if needed
+                                storageRef.downloadUrl.addOnSuccessListener { downloadUri ->
+                                    tempFile?.delete()
+                                    Toast.makeText(
+                                        requireContext(),
+                                        "Audio uploaded. URI: $downloadUri",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    val obs : Observation = Observation(null, encodedBitmap, "Unspecified Species", "I don't know", downloadUri.toString(), latitude, longitude, output)
+                                    //GlobalVariablesMethods.user.categories?.add(cat)
+                                    val database = FirebaseDatabase.getInstance()
+                                    val databaseReference = database.getReference("Users")
+                                    //code attribution
+                                    //the following code was taken from Stack Overflow and adapted
+                                    //https://stackoverflow.com/questions/60432256/on-insert-data-in-firebase-realtime-database-it-deletes-previous-data
+                                    //ashok
+                                    //https://stackoverflow.com/users/12746098/ashok
+                                    databaseReference.child(user?.uid.toString()).child("observations").push().setValue(obs).addOnCompleteListener() {
+                                        if (it.isComplete){
+                                            Toast.makeText(context, "Observation added successfully.", Toast.LENGTH_SHORT).show()
+                                        }
+                                        else{
+                                            Toast.makeText(context, "Observation could not be added.", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }.addOnCompleteListener(){
+                                        dismiss()
+                                    }.addOnFailureListener(){
+                                        Toast.makeText(context, "Failure", Toast.LENGTH_SHORT).show()
+                                    }
+
+                                }
+                            } else {
+                                Toast.makeText(requireContext(), "Error uploading audio: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
                 }
                 catch (ex : Exception){
                     Toast.makeText(context, ex.message, Toast.LENGTH_SHORT).show()
@@ -164,12 +236,12 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
         startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
     }
 
-    private val requestPermissionsLauncher =
+    private val requestMediaPermissionsLauncher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-            if (isPermissionGranted(permissions, getReadStoragePermission())) {
+            if (isMediaPermissionGranted(permissions, getReadStoragePermission())) {
                 openImagePicker()
             } else {
-                // Handle permission denied
+                Toast.makeText(context, "Media permissions have been denied.", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -177,10 +249,31 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
         val permissionsToRequest = arrayOf(getReadStoragePermission())
 
         // Request multiple permissions to access images
-        requestPermissionsLauncher.launch(permissionsToRequest)
+        requestMediaPermissionsLauncher.launch(permissionsToRequest)
+    }
+
+    private val audioPermissions = arrayOf(
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    private val requestAudioPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (isAudioPermissionGranted(permissions, android.Manifest.permission.RECORD_AUDIO)) {
+                startRecording()
+            } else {
+                // Handle permission denial
+                Toast.makeText(requireContext(), "Audio permissions have been denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    private fun requestAudioPermissions() {
+        val permissionsToRequest = audioPermissions
+
+        // Request multiple permissions to access images
+        requestAudioPermissionsLauncher.launch(permissionsToRequest)
     }
 
     private fun openImagePicker() {
+        // Launch the image picker
         // Launch the image picker
         getContentLauncher.launch("image/*")
     }
@@ -193,7 +286,17 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
         }
     }
 
-    private fun isPermissionGranted(permissions: Map<String, Boolean>, permission: String): Boolean {
+    private fun isMediaPermissionGranted(
+        permissions: Map<String, Boolean>,
+        permission: String
+    ): Boolean {
+        return permissions[permission] == true
+    }
+
+    private fun isAudioPermissionGranted(
+        permissions: Map<String, Boolean>,
+        permission: String
+    ): Boolean {
         return permissions[permission] == true
     }
 
@@ -213,36 +316,6 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
 //                // Do something with the selected image URI, such as displaying it or processing it.
 //            }
         }
-
-    // Old code does not work properly for newer versions
-//    private fun isReadStoragePermissionGranted(): Boolean {
-//        return ContextCompat.checkSelfPermission(
-//            requireContext(),
-//            Manifest.permission.READ_EXTERNAL_STORAGE
-//        ) == PackageManager.PERMISSION_GRANTED
-//    }
-//
-//    private fun requestReadStoragePermission() {
-//        ActivityCompat.requestPermissions(
-//            requireActivity(),
-//            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-//            REQUEST_OPEN_IMAGE
-//        )
-//    }
-//
-//    private fun openImagePicker() {
-//        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-//        startActivityForResult(intent, REQUEST_OPEN_IMAGE)
-//    }
-
-//    fun addData(){
-//        if(imgCategory.drawable == null){
-//            imageString = null
-//        }
-//        else{
-//            imageString = encodedBitmap.toString()
-//        }
-//    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -264,13 +337,147 @@ class UnidentifiedDialogFragment(private val latitude : String, private val long
 //            }
 //        }
 //        else{
-            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-                val imageBitmap = data?.extras?.get("data") as Bitmap
-                image_bird.background = null
-                image_bird.setImageBitmap(imageBitmap)
-                encodedBitmap = GlobalMethods.encodeImage(imageBitmap)
-                // Do something with the captured image, such as displaying it or saving it.
-            }
+        if (requestCode == BirdManualEntryDialogFragment.REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            image_bird.background = null
+            image_bird.setImageBitmap(imageBitmap)
+            encodedBitmap = GlobalMethods.encodeImage(imageBitmap)
+            // Do something with the captured image, such as displaying it or saving it.
+        }
 //        }
+    }
+
+    private fun toggleRecording() {
+        if (!isRecording) {
+            requestAudioPermissions()
+        } else {
+            stopRecording()
+        }
+    }
+
+    private fun togglePlaySound() {
+        try{
+            if (!isPlaying) {
+                if(tempFile!=null && tempFile?.length() != 0L){
+                    playAudio(tempFile!!)
+                }
+                else{
+                    Toast.makeText(context, "Please record a sound", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                stopPlaying()
+            }
+        }
+        catch (e:Exception){
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun startRecording() {
+        try {
+            tempFile = createTempFile("audio_record_${System.currentTimeMillis()}", ".mp3")
+            mediaRecorder = MediaRecorder().apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+                setOutputFile(tempFile!!.absolutePath)
+                prepare()
+                start()
+            }
+
+            recordingChronometer.base = SystemClock.elapsedRealtime()
+            recordingChronometer.start()
+            isRecording = true
+            btnPlay.isEnabled = false
+            tv_record_sound.setText("Stop Recording")
+        } catch (e: IOException) {
+            Toast.makeText(requireContext(), "Error starting recording: ${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun stopRecording() {
+        try{
+            mediaRecorder?.apply {
+                stop()
+                release()
+            }
+            recordingChronometer.stop()
+            mediaRecorder = null
+            btnPlay.isEnabled = true
+            tv_record_sound.setText("Record Sound")
+            isRecording = false
+            Toast.makeText(context, "You can play the audio now.", Toast.LENGTH_SHORT).show()
+        }
+        catch (e:Exception){
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun stopPlaying(){
+        mediaPlayer?.apply {
+            stop()
+            release()
+        }
+        playbackChronometer.stop()
+        playbackChronometer.base = SystemClock.elapsedRealtime()
+        mediaPlayer = null
+
+        tv_play_sound.setText("Play Sound")
+        isPlaying = false
+    }
+
+    private fun playAudio(file: File) {
+        try{
+            if(tempFile != null && tempFile?.length() != 0L){
+                try {
+                    mediaPlayer = MediaPlayer().apply {
+                        reset()
+                        setDataSource(file.absolutePath)
+                        prepare()
+                        start()
+
+                    }
+                    mediaPlayer?.setOnErrorListener { mp, what, extra ->
+                        Toast.makeText(context, "Please note the media player is still preparing.", Toast.LENGTH_SHORT).show()
+                        false
+                    }
+
+                    // Set a completion listener to reset the MediaPlayer when playback completes
+                    mediaPlayer?.setOnCompletionListener {
+                        mediaPlayer?.reset()
+                        tv_play_sound.text = "Play Sound"
+                        playbackChronometer.stop()
+                        playbackChronometer.base = SystemClock.elapsedRealtime()
+                    }
+
+                    // Set a prepared listener to start playback when prepared
+                    mediaPlayer?.setOnPreparedListener {
+                        mediaPlayer?.start()
+                    }
+
+                    playbackChronometer.base = SystemClock.elapsedRealtime()
+                    playbackChronometer.start()
+                    tv_play_sound.setText("Stop Playing")
+                    isPlaying = true
+                } catch (e: IOException) {
+                    Toast.makeText(requireContext(), "Error playing audio: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(context, "Please record a sound", Toast.LENGTH_SHORT).show()
+            }
+        }
+        catch (e:Exception){
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release()
+        mediaPlayer = null
+        mediaRecorder?.release()
+        mediaRecorder = null
     }
 }
